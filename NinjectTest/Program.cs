b2a06f4538b31f410f9614f2b3c0d69d82d8f5ca
Alex <alexs1305@gmail.com>
2016-01-31 21:34:06 +0000

    refactor(program.cs): seperated out dependency and ninject differences

--- NinjectTest/Program.cs
+++ NinjectTest/Program.cs
@@ -9,7 +9,31 @@ namespace NinjectTest
 {
     class Program : NinjectSettings
     {
+        private static IPlayer[] Players;
+        private static ICompetition Competition;
+
         static void Main(string[] args)
+        {
+            //Standard Dependency Injection
+            var players = new IPlayer[2];
+            players[0] = new Alex();
+            players[1] = new NewPlayer();
+
+            DependencyInjection(players);
+
+            //Ninject
+            Ninject();
+
+        }
+
+        public static void DependencyInjection(IPlayer[] players)
+        {
+            Players = players;
+            Competition = new PoolCompetition(Players);
+            Competition.Players.ToList().ForEach(p => Console.WriteLine($"Standard Dependency Injection adding {p.Name}"));
+        }
+
+        public static void Ninject()
         {
             IKernel kernal = new StandardKernel(new PlayerBindingModule());
 
